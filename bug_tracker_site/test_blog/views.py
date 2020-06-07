185ca989d766d5e9ff20f8c29862d752a51d2930
JKrystofik <jkrystofik2290@hotmail.com>
2020-06-07 14:05:10 -0400

    Populated new database model and added to test_blog/views.py

--- bug_tracker_site/test_blog/views.py
+++ bug_tracker_site/test_blog/views.py
@@ -1,23 +1,12 @@
 """Test_Blog django views handler"""
 from django.shortcuts import render
 from django.http import HttpResponse
-
-posts = [{
-    'author': 'JoeK',
-    'title': 'My First Post',
-    'content': 'Some stuff to blog about.',
-    'date_posted': '6/5/2020 3:30pm'
-}, {
-    'author': 'MattK',
-    'title': "Matt's Blog Post",
-    'content': 'Matt likes to cook!',
-    'date_posted': '6/6/2020 10:23am'
-}]
+from .models import BlogPost
 
 
 def index(request):
     """Serves the index page of test_blog app"""
-    context = {'posts': posts}
+    context = {'posts': BlogPost.objects.all()}  # pylint: disable='no-member'
     return HttpResponse(render(request, 'test_blog/index.html', context))
 
 
