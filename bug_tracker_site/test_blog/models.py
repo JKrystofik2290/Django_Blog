38b0e29c5da37ae9e438e18b689585455993d9dc
JKrystofik <jkrystofik2290@hotmail.com>
2020-06-07 13:26:23 -0400

    Created database models

--- bug_tracker_site/test_blog/models.py
+++ bug_tracker_site/test_blog/models.py
@@ -1,3 +1,12 @@
+"""Models for database"""
 from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class BlogPost(models.Model):
+    """DB model for blog posts"""
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    date_posted = models.DateTimeField(default=timezone.now)
+    title = models.CharField(max_length=100)
+    content = models.TextField()
