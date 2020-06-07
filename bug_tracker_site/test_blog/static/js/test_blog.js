d909dd32593ae184c2d5572c159d2acffdb708aa
JKrystofik <jkrystofik2290@hotmail.com>
2020-06-07 17:08:38 -0400

    Added javascript to test_blog app that offsets from UTC time to client timezone.

--- bug_tracker_site/test_blog/static/js/test_blog.js
+++ bug_tracker_site/test_blog/static/js/test_blog.js
@@ -1,1 +1,6 @@
-document.getElementById("post_time").innerHTML = document.getElementById("post_time").innerHTML + ' (UTC)'
+// Offsets UTC blog post times with client timezone
+[...document.getElementsByClassName("post_time")].forEach(time => {
+    var time_offset = new Date().getTimezoneOffset() / 60;
+    var new_hour = parseInt(time.innerHTML.split(":")[0]) - time_offset;
+    time.innerHTML = new_hour + ":" + time.innerHTML.split(":")[1];
+});
