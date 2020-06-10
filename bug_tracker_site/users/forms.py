9c98130e9b584d4eaeee8e194b978b5b846ae713
Krystofik <joe.krystofik@crown.com>
2020-06-10 14:56:53 -0400

    Added user ifo update form and restricted profile pic file size.

--- bug_tracker_site/users/forms.py
+++ bug_tracker_site/users/forms.py
@@ -2,6 +2,7 @@
 from django import forms
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
+from .models import Profile
 
 
 class UserRegisterForm(UserCreationForm):
@@ -12,3 +13,21 @@ class UserRegisterForm(UserCreationForm):
         """Nested namespace for configurations"""
         model = User
         fields = ['username', 'email', 'password1', 'password2']
+
+
+class UserUpdateForm(forms.ModelForm):
+    """Class for updating user info"""
+    email = forms.EmailField()
+
+    class Meta:
+        """Nested namespace for configurations"""
+        model = User
+        fields = ['username', 'email']
+
+
+class ProfileUpdateForm(forms.ModelForm):
+    """Class for updating user image"""
+    class Meta:
+        """Nested namespace for configurations"""
+        model = Profile
+        fields = ['image']
