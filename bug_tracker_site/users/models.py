d46207f920e922c0dc8d9c59bc412f3891be9305
Krystofik <joe.krystofik@crown.com>
2020-06-10 13:12:10 -0400

    Added Profile to database

--- bug_tracker_site/users/models.py
+++ bug_tracker_site/users/models.py
@@ -1,3 +1,13 @@
+"""Models for Users database"""
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Profile(models.Model):
+    """Database model for user profiles"""
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    image = models.ImageField(default='default_user.jpg',
+                              upload_to='profile_pics')
+
+    def __str__(self):
+        return f'{self.user.username} Profile'
