8a9215b40a7ce5dcef5c17255f33a3a4b1bfcbd4
Krystofik <joe.krystofik@crown.com>
2020-06-11 09:08:08 -0400

    added class views and idividual post view

--- bug_tracker_site/test_blog/urls.py
+++ bug_tracker_site/test_blog/urls.py
@@ -1,8 +1,10 @@
 """Test Blog URLs"""
 from django.urls import path
+from .views import PostListView, PostDetailView
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='test_blog-home'),
+    path('', PostListView.as_view(), name='test_blog-home'),
+    path('post/<int:pk>/', PostDetailView.as_view(), name='post-detail'),
     path('about/', views.about, name='test_blog-about'),
 ]
--- bug_tracker_site/test_blog/views.py
+++ bug_tracker_site/test_blog/views.py
@@ -1,16 +1,30 @@
 """Test_Blog django views handler"""
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.views.generic import ListView, DetailView
 from .models import BlogPost
 
 
 def index(request):
     """Serves the index page of test_blog app"""
     context = {'posts': BlogPost.objects.all()}  # pylint: disable='no-member'
-    return HttpResponse(render(request, 'test_blog_index.html', context))
+    return render(request, 'test_blog_index.html', context)
+
+
+class PostListView(ListView):
+    """Serves blog home view"""
+    model = BlogPost
+    template_name = 'test_blog_index.html'
+    context_object_name = 'posts'
+    ordering = ['-date_posted', '-time_posted']
+
+
+class PostDetailView(DetailView):
+    """Serves individual blog post view"""
+    model = BlogPost
+    template_name = 'post_detail.html'
 
 
 def about(request):
     """Serves the about page of test_blog app"""
-    return HttpResponse(
-        render(request, 'test_blog_about.html', {'title': 'About Test_Blog'}))
+    return render(request, 'test_blog_about.html',
+                  {'title': 'About Test_Blog'})
