ec2afa888b3772dc94ba5a7bf0b12e0b4b3a554f
JKrystofik <jkrystofik2290@hotmail.com>
2020-06-12 18:35:44 -0400

    setup AWS S3

--- blog_site/users/models.py
+++ blog_site/users/models.py
@@ -1,7 +1,7 @@
 """Models for Users database"""
 from django.db import models
 from django.contrib.auth.models import User
-# from PIL import Image
+from PIL import Image
 
 
 class Profile(models.Model):
