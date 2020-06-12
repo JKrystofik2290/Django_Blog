8e165bec850f3576d96cd7b556d4166338807f0d
Krystofik <joe.krystofik@crown.com>
2020-06-12 11:09:36 -0400

    working on email password reset

--- blog_site/site_manager/urls.py
+++ blog_site/site_manager/urls.py
@@ -32,15 +32,15 @@ urlpatterns = [
     path('password-reset/',
          auth_views.PasswordResetView.as_view(
              template_name='password_reset.html'),
-         name='password-reset'),
-    path('password-reset/done',
+         name='password_reset'),
+    path('password-reset/done/',
          auth_views.PasswordResetDoneView.as_view(
              template_name='password_reset_done.html'),
-         name='password_reset-done'),
+         name='password_reset_done'),
     path('password-reset-confirm/<uidb64>/<token>',
          auth_views.PasswordResetConfirmView.as_view(
              template_name='password_reset_confirm.html'),
-         name='password_reset-confirm'),
+         name='password_reset_confirm'),
     path('register/', user_views.register, name="user-reg"),
     path('profile/', user_views.profile, name="user-profile"),
 ]
