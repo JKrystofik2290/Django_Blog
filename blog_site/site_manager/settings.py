ac66accff831a89d99e8c436252ccccf7bdb973e
JKrystofik <jkrystofik2290@hotmail.com>
2020-06-12 17:40:28 -0400

    Fixing bugs

--- blog_site/site_manager/settings.py
+++ blog_site/site_manager/settings.py
@@ -19,25 +19,21 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('MY_PY_KEY')
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get('DEBUG')
+DEBUG = True
 
-ALLOWED_HOSTS = ['my-test-django-blog.herokuapp.com']
+# ALLOWED_HOSTS = ['my-test-django-blog.herokuapp.com']
+ALLOWED_HOSTS = []
 
 # Application definition
 
 INSTALLED_APPS = [
-    'blog.apps.BlogConfig',
-    'users.apps.UsersConfig',
-    'crispy_forms',
-    'django.contrib.admin',
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.messages',
-    'django.contrib.staticfiles',
+    'blog.apps.BlogConfig', 'users.apps.UsersConfig', 'crispy_forms',
+    'django.contrib.admin', 'django.contrib.auth',
+    'django.contrib.contenttypes', 'django.contrib.sessions',
+    'django.contrib.messages', 'django.contrib.staticfiles', 'storages'
 ]
 
 MIDDLEWARE = [
@@ -118,7 +114,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
@@ -136,3 +132,11 @@ EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = os.environ.get('GMAIL_USER')
 EMAIL_HOST_PASSWORD = os.environ.get('GMAIL_PASS')
+
+AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
+AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
+AWS_STORAGE_BUCKET_NAME = os.environ.get('AWS_STORAGE_BUCKET_NAME')
+
+AWS_S3_FILE_OVERWRITE = False
+AWS_DEFAULT_ACL = None
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
--- blog_site/users/models.py
+++ blog_site/users/models.py
@@ -1,7 +1,7 @@
 """Models for Users database"""
 from django.db import models
 from django.contrib.auth.models import User
-from PIL import Image
+# from PIL import Image
 
 
 class Profile(models.Model):
@@ -13,13 +13,13 @@ class Profile(models.Model):
     def __str__(self):
         return f'{self.user.username} Profile'  # pylint: disable='no-member'
 
-    def save(self, *args, **kwargs):
-        """Overwritting parent save method"""
-        super().save(*args, **kwargs)
+    # def save(self, *args, **kwargs):
+    #     """Overwritting parent save method"""
+    #     super().save(*args, **kwargs)
 
-        img = Image.open(self.image.path)  # pylint: disable='no-member'
+    #     img = Image.open(self.image.path)  # pylint: disable='no-member'
 
-        if img.height > 300 or img.width > 300:
-            output_size = (300, 300)
-            img.thumbnail(output_size)
-            img.save(self.image.path)  # pylint: disable='no-member'
+    #     if img.height > 300 or img.width > 300:
+    #         output_size = (300, 300)
+    #         img.thumbnail(output_size)
+    #         img.save(self.image.path)  # pylint: disable='no-member'
